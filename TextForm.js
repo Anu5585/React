3bf79c30309c1c97daac15887923e7820a5f2985
Annas5585 <105589036+Anu5585@users.noreply.github.com>
2024-04-13 19:28:21 +0500

    Update TextForm.js

--- TextForm.js
+++ TextForm.js
@@ -15,21 +15,30 @@ export default function TextForm(props) {
         setText(newText);
 
     }
-    const handleOnChange = (event)=>{
+    const handleOnChange = (event)=>{  
 
         // console.log("On Change")
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
   return (
-    <div>
+    <>
+    <div className='container'>
       <h1>{props.heading} </h1>
 
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div> 
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Case</button>
-      <button className="btn btn-primary" onClick={handleDownClick}>Convert to Lower Case</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
+      <button className="btn btn-primary mx-1" onClick={handleDownClick}>Convert to Lower Case</button>
     </div>
+    <div className="container my-3">
+      <h1>Your Text Summary </h1>
+      <p>{text.split(" ").length} words and {text.length} characters </p>
+      <p>{0.008 * text.split(" ").length} Minutes To Read  </p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
